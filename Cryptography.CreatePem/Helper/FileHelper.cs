cc031eec5fedc2a653c4118c3a824165ea4ce2a5
Renan Carvalho <renan@interprocess.com.br>
2022-07-28 17:15:51 -0300

    feat: Add console application create pem

---
+++ Cryptography.CreatePem/Helper/FileHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Cryptography.CreatePem.Helper
+{
+    public class FileHelper
+    {
+        public static bool StringToFile(string path, string fileString)
+        {
+            try
+            {
+
+                File.WriteAllText(path, fileString);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+                return false;
+            }
+        }
+    }
+}
