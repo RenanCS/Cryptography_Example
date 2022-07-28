fbd0cbbb0818325ceaa545d4ed340dca1b66f599
Renan Carvalho <renan@interprocess.com.br>
2022-07-28 17:16:31 -0300

    feat: add console application

---
+++ CryptographyCreatePem/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Cryptography.Library;
+using Cryptography.Library.Helper;
+using CryptographyCreatePem.Helper;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewRSA rsaParams = new NewRSA();
+
+            string pathPublicKey = "C:\\keys\\public.perm";
+            string pathPrivateKey = "C:\\keys\\private.perm.";
+
+            if (!File.Exists(pathPrivateKey))
+            {
+                FileHelper.StringToFile(pathPublicKey, WriteReadKey.ExportPublicKey(rsaParams.rsa));
+                FileHelper.StringToFile(pathPrivateKey, WriteReadKey.ExportPrivateKey(rsaParams.rsa));
+            }
+
+            WriteReadKey.GetPrivateKeyFromPemFile(pathPrivateKey, rsaParams.rsa);
+            WriteReadKey.GetPublicKeyFromPemFile(pathPublicKey, rsaParams.rsa);
+        }
+    }
+}
+
+
