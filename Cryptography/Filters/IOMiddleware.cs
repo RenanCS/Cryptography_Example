b1b4453cff8917d6a5a100a128d7633fe0f6a2d9
Renan Carvalho <renan@interprocess.com.br>
2022-07-29 12:41:01 -0300

    chore: Change IOMiddleware for stable

--- Cryptography/Filters/IOMiddleware.cs
+++ Cryptography/Filters/IOMiddleware.cs
@@ -36,8 +36,11 @@ namespace Cryptography.Filters
 
                 if (!string.IsNullOrEmpty(body))
                 {
-                    var newBody = DecryptBlock(body);
+                    //var newBody = DecryptBlock(body);
+                    var newBody = DecryptBlockSimple(body);
+
                     request.Body = new MemoryStream(Encoding.UTF8.GetBytes(newBody));
+                    request.Headers.ContentType = "application/json";
                 }
 
             }
@@ -70,7 +73,9 @@ namespace Cryptography.Filters
                 {
                     string body = await bufferReader.ReadToEndAsync();
 
-                    newBody = EncryptStream(body);
+                    //newBody = EncryptStream(body);
+                    newBody = EncryptStreamSimple(body);
+
                     //reset read to start of stream
                     buffer.Seek(0, SeekOrigin.Begin);
 
@@ -80,6 +85,9 @@ namespace Cryptography.Filters
                     //copy our content to the original stream and put it back
                     await buffer.CopyToAsync(stream);
                     context.Response.Body = stream;
+
+                    // Sempre retorna um text/plain
+                    context.Response.ContentType = "text/plain";
 
                 }
             }
@@ -94,12 +102,26 @@ namespace Cryptography.Filters
             return encryptedBlockJson;
         }
 
+        private static string EncryptStreamSimple(string original)
+        {
+            var encryptedBlock = rsaParams.Encrypt(original);
+            var encryptedBlockJson = Convert.ToBase64String(encryptedBlock);
+            return encryptedBlockJson;
+        }
+
         private static string DecryptBlock(string responseBody)
         {
             EncryptedPacket encryptedBlock = JsonConvert.DeserializeObject<EncryptedPacket>(responseBody);
             var decrpyted = hybrid.DecryptData(encryptedBlock, rsaParams, digitalSignature);
             var decrpytedString = Encoding.UTF8.GetString(decrpyted);
 
+            return decrpytedString;
+        }
+
+        private static string DecryptBlockSimple(string responseBody)
+        {
+            var decrpyted = rsaParams.Decrypt(Convert.FromBase64String(responseBody));
+            var decrpytedString = Encoding.UTF8.GetString(decrpyted);
             return decrpytedString;
         }
 
@@ -113,8 +135,9 @@ namespace Cryptography.Filters
                 throw new Exception("Não foi encontrado as chaves públicas e privadas");
             }
 
+            // OBS: A importação da chave privada sempre tem que ser a última
+            WriteReadKey.GetPublicKeyFromPemFile(pathPublicKey, rsaParams.rsa);
             WriteReadKey.GetPrivateKeyFromPemFile(pathPrivateKey, rsaParams.rsa);
-            WriteReadKey.GetPublicKeyFromPemFile(pathPublicKey, rsaParams.rsa);          
         }
     }
 }
