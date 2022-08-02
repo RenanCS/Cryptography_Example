f4b2958b8004e7d23b8316b2dda53b5bd1fbb81c
Renan Carvalho <renan@interprocess.com.br>
2022-08-02 10:03:22 -0300

    feat: add interface base

---
+++ Cryptography.Library/Interface/ICryptography.cs
@@ -0,0 +1,8 @@
+﻿namespace Cryptography.Library.Interface
+{
+    public interface ICryptography
+    {
+        public string Encrypt(string data);
+        public string Decrypt(string data);
+    }
+}
